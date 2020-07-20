f8d03837993437bb38011adcc90ac7bf51f143f7
ferrisbueller66 <github email address>
2020-07-20 10:07:22 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -7,14 +7,23 @@ export default class ColorBox extends Component {
       
     ]
   }
+  let newValue = parseFloat(this.props.value) - .1
 
   render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: 1}}>
+        { 
+        return this.props.value < .2 ? null : (<div><className="color-box" style={{opacity: newValue}}/div>)
+        }
       </div>
     )
   }
 
 }
 
+//let box = document.querySelector('.color-box')
+//box.style.opacity = 1
+//let newValue = parseFloat(box.style.opacity) - .1
+//box.style.opacity = newValue
+
+//repeat 3 and 4 until reach value of .1
